c5129bae904e82922616b2681b7a5deb569e3a73
Likhit <likhitgmella@gmail.com>
2020-05-24 16:19:31 +0530

    login screen #1

---
+++ Swiftagram/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 24/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
+
+// Dismiss keyboard on tapping anywhere
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}   // #35
---
+++ Swiftagram/SignInViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SignInViewController.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 24/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var signInOutLet: UIButton!
+    @IBOutlet var signUpOutLet: UIButton!
+    
+    func Colors() {
+        
+        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        signInOutLet.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        signUpOutLet.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        
+    }
+    
+    func CornerRadius() {
+        
+        // Textfield Corner Radius Property
+        emailTextField.layer.cornerRadius = 4
+        passwordTextField.layer.cornerRadius = 4
+        signInOutLet.layer.cornerRadius = 6
+        signUpOutLet.layer.cornerRadius = 6
+        
+    }
+    
+    func LeftPadding() {
+        
+        // Create a padding view for Credits TextFields on LEFT
+        emailTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: emailTextField.frame.height))
+        emailTextField.leftViewMode = .always
+        passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: passwordTextField.frame.height))
+        passwordTextField.leftViewMode = .always
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Colors()
+        hideKeyboardWhenTappedAround()
+        CornerRadius()
+        LeftPadding()
+        
+    }
+
+}   // #58
