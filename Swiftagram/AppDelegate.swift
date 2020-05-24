f404f0fcb7e1597f1d74ba8a33fd04a3fc0e0246
Likhit <likhitgmella@gmail.com>
2020-05-24 20:30:22 +0530

    nav bar setup

--- Swiftagram/AppDelegate.swift
+++ Swiftagram/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().barTintColor = .white
+        UITabBar.appearance().tintColor = .black
         return true
     }
 
@@ -38,4 +40,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-}   // #42
+}   // #44
