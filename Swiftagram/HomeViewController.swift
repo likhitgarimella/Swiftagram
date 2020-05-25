8a0255f76fb6fb26163e178168ded95b8135594a
Likhit <likhitgmella@gmail.com>
2020-05-26 04:58:09 +0530

    logout functionality

--- Swiftagram/HomeViewController.swift
+++ Swiftagram/HomeViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // nav bar button
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         
+    }
+    
+    @objc func handleLogout() {
+        
+        do {
+            try Auth.auth().signOut()
+            print("----------------")
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let signInVC = storyboard.instantiateViewController(withIdentifier: "SignInViewController")
+        self.present(signInVC, animated: true, completion: nil)
         
     }
 
-}   // #21
+}   // #38
