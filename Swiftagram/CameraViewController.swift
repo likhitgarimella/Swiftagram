1315e9ca47e1efd7e4916b80489fb818b1647c75
Likhit <likhitgmella@gmail.com>
2020-05-26 18:18:23 +0530

    error checking while sharing posts

--- Swiftagram/CameraViewController.swift
+++ Swiftagram/CameraViewController.swift
@@ -47,8 +47,10 @@ class CameraViewController: UIViewController, UITextViewDelegate {
     
     func Properties() {
         
+        shareOutlet.layer.cornerRadius = 6
+        
         captionTextView.backgroundColor = UIColor.white
-        captionTextView.delegate = self
+        captionTextView.delegate = self     // text view delegate
         captionTextView.text = "Write a caption..."
         captionTextView.textColor = UIColor.lightGray
         
@@ -65,7 +67,29 @@ class CameraViewController: UIViewController, UITextViewDelegate {
         // To get access to selected media files, add delegate
         pickerController.delegate = self
         // present photo library
+        pickerController.modalPresentationStyle = .fullScreen
         present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        handlePost()
+        
+    }
+    
+    func handlePost() {
+        
+        if selectedImage != nil {
+            self.shareOutlet.isEnabled = true
+            shareOutlet.setTitleColor(UIColor.white, for: .normal)
+            shareOutlet.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        } else {
+            self.shareOutlet.isEnabled = false
+            shareOutlet.setTitleColor(UIColor.lightText, for: .normal)
+            shareOutlet.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        }
         
     }
     
@@ -131,7 +155,11 @@ class CameraViewController: UIViewController, UITextViewDelegate {
             self.hud1.show(in: self.view)
             self.hud1.indicatorView = nil    // remove indicator
             self.hud1.textLabel.text = "Success!"
+            self.photo.image = UIImage(named: "Placeholder-image")
+            self.selectedImage = nil
+            self.captionTextView.textColor = UIColor.lightGray
             self.hud1.dismiss(afterDelay: 2.0, animated: true)
+            self.tabBarController?.selectedIndex = 0
         })
         
     }
@@ -152,4 +180,4 @@ extension CameraViewController: UIImagePickerControllerDelegate, UINavigationCon
         dismiss(animated: true, completion: nil)
     }
     
-}   // #156
+}   // #184
