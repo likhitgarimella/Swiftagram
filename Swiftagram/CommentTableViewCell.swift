5bd8864ad5fdd158772dde55f9a2096d9e675c56
Likhit <likhitgmella@gmail.com>
2020-05-30 20:34:28 +0530

    comment screen add classes

---
+++ Swiftagram/CommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentTableViewCell.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 30/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}   // 24
---
+++ Swiftagram/CommentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CommentViewController.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 30/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}   // #21
