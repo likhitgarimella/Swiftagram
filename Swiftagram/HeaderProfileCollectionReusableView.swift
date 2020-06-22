55a48ae524a84f6c8050c330beb7cd22edb1986a
Likhit <likhitgmella@gmail.com>
2020-06-22 05:36:12 +0530

    profile screen setup #5

---
+++ Swiftagram/HeaderProfileCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderProfileCollectionReusableView.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 22/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class HeaderProfileCollectionReusableView: UICollectionReusableView {
+        
+}   // #14
