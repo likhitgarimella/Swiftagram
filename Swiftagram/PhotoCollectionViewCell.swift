c9a9a58c73edc4723763922f93b296a9c24bef9e
Likhit <likhitgmella@gmail.com>
2020-07-01 07:45:38 +0530

    profile post cell setup

---
+++ Swiftagram/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 01/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}   // #14
