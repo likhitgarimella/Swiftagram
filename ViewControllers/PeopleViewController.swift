baf3621349a0dc8c38082bd15f81ff11d7094e83
Likhit <likhitgmella@gmail.com>
2020-07-05 10:53:55 +0530

    pod update + people VC files setup

---
+++ ViewControllers/PeopleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PeopleViewController.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 05/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Views/PeopleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PeopleTableViewCell.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 05/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+
+}   // #28
