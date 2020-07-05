4af1aab098d6000abf2d2d471089f7e1f34ecbde
Likhit <likhitgmella@gmail.com>
2020-07-05 22:56:40 +0530

    move follow and unfollow actions to FollowApi

--- Swiftagram/FollowApi.swift
+++ Swiftagram/FollowApi.swift
@@ -14,4 +14,14 @@ class FollowApi {
     var REF_FOLLOWERS = Database.database().reference().child("followers")
     var REF_FOLLOWING = Database.database().reference().child("following")
     
-}   // #18
+    func followAction(withUser id: String) {
+        REF_FOLLOWERS.child(id).child(Api.UserDet.CURRENT_USER!.uid).setValue(true)
+        REF_FOLLOWING.child(Api.UserDet.CURRENT_USER!.uid).child(id).setValue(true)
+    }
+    
+    func unFollowAction(withUser id: String) {
+        REF_FOLLOWERS.child(id).child(Api.UserDet.CURRENT_USER!.uid).setValue(NSNull())
+        REF_FOLLOWING.child(Api.UserDet.CURRENT_USER!.uid).child(id).setValue(NSNull())
+    }
+    
+}   // #28
--- Views/PeopleTableViewCell.swift
+++ Views/PeopleTableViewCell.swift
@@ -29,22 +29,20 @@ class PeopleTableViewCell: UITableViewCell {
             profileImage.sd_setImage(with: photoUrl, placeholderImage: UIImage(named: "Placeholder-image"))
         }
         
-        followButton.addTarget(self, action: #selector(self.followAction), for: .touchUpInside)
+        // followButton.addTarget(self, action: #selector(self.followAction), for: .touchUpInside)
         followButton.addTarget(self, action: #selector(self.unFollowAction), for: .touchUpInside)
         
     }
     
     @objc func followAction() {
         
-        Api.Follow.REF_FOLLOWERS.child(user!.id!).child(Api.UserDet.CURRENT_USER!.uid).setValue(true)
-        Api.Follow.REF_FOLLOWING.child(Api.UserDet.CURRENT_USER!.uid).child(user!.id!).setValue(true)
+        Api.Follow.followAction(withUser: user!.id!)
         
     }
     
     @objc func unFollowAction() {
         
-        Api.Follow.REF_FOLLOWERS.child(user!.id!).child(Api.UserDet.CURRENT_USER!.uid).setValue(NSNull())
-        Api.Follow.REF_FOLLOWING.child(Api.UserDet.CURRENT_USER!.uid).child(user!.id!).setValue(NSNull())
+        Api.Follow.unFollowAction(withUser: user!.id!)
         
     }
     
@@ -62,4 +60,4 @@ class PeopleTableViewCell: UITableViewCell {
         
     }
 
-}   // #66
+}   // #64
