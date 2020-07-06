18a85d48d9400e69b716105430f8333b4bfc47e6
Likhit <likhitgmella@gmail.com>
2020-07-06 20:26:09 +0530

    FeedApi

---
+++ Swiftagram/FeedApi.swift
@@ -0,0 +1,27 @@
+//
+//  FeedApi.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 06/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FeedApi {
+    
+    var REF_FEED = Database.database().reference().child("feed")
+    
+    func observeFeed(withId id: String, completion: @escaping (Post) -> Void) {
+        REF_FEED.child(id).observe(.childAdded, with: {
+            snapshot in
+            let key = snapshot.key
+            Api.Post.observePost(withId: key, completion: {
+                (post) in
+                completion(post)
+            })
+        })
+    }
+    
+}   // #28
