0446a1e1175489a5cf8936882a5d16517269621f
Likhit <likhitgmella@gmail.com>
2020-07-15 23:46:56 +0530

    search VC setup

---
+++ ViewControllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Swiftagram
+//
+//  Created by Likhit Garimella on 15/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}   // #21
